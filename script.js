1557ff555d612c9725dabfcd4ff0b7c6d66c6cf7
Sulfurous-Impersonation <june@watson-net.com>
2023-04-03 22:10:29 -0700

    messing with html/css/javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+function myFunction() 
+{
+	document.getElementById("demo").innerHTML = "Hello World";
+}
+
+function streetratChargen()
+{
+	//idk, figure out how to change the page to 
+	document.getElementById("demo").innerHTML = "STREETRATS";
+}
+
+function edgerunnerChargen()
+{
+	//idk, figure out how to change the page to
+	document.getElementById("demo").innerHTML = "EDGERUNNERS";
+}
+
+function completeChargen()
+{
+	//idk, figure out how to change the page to 
+	document.getElementById("demo").innerHTML = "COMPLETE PACKAGES";
+	
+}
