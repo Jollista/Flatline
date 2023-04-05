c750c3f4010eabcc0cd9d222344317a4a6ffd2b8
Sulfurous-Impersonation <june@watson-net.com>
2023-04-05 00:49:07 -0700

    trying to add page changing

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-function myFunction() 
-{
-	document.getElementById("demo").innerHTML = "Hello World";
-}
-
-function streetratChargen()
-{
-	//idk, figure out how to change the page to 
-	document.getElementById("demo").innerHTML = "STREETRATS";
-}
-
-function edgerunnerChargen()
-{
-	//idk, figure out how to change the page to
-	document.getElementById("demo").innerHTML = "EDGERUNNERS";
-}
-
-function completeChargen()
-{
-	//idk, figure out how to change the page to 
-	document.getElementById("demo").innerHTML = "COMPLETE PACKAGES";
-}
+document.getElementById("completeButton").addEventListener("click", function(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	  if (this.readyState == 4 && this.status == 200) {
+	    document.getElementById("content").innerHTML = this.responseText;
+	  }
+	};
+	xhttp.open("GET", "/completeChargen.html", true);
+	xhttp.send();
+      });
